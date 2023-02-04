339ee97c9b92e910c2e1500e7ea5c34cad3bba9f
finalmegaman NA <finalmegaman@gmail.com>
2023-02-04 14:29:29 -0800

    29 Grid Object Debug.

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -7,30 +7,48 @@ public class GridSystem
     private int width;
     private int height;
     private float cellSize;
+    private GridObject[,] gridObjectArray;
 
-    public GridSystem(int width, int height, float cellSize) {
+    public GridSystem(int width, int height, float cellSize)
+    {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+
+        gridObjectArray = new GridObject[width, height];
 
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
             }
         }
-        
+
     }
 
-    public Vector3 GetWorldPosition(int x, int z) {
+    public Vector3 GetWorldPosition(int x, int z)
+    {
         return new Vector3(x, 0, z) * cellSize;
     }
 
-    public GridPosition GetGridPosition(Vector3 worldPosition) {
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
         return new GridPosition(
             Mathf.RoundToInt(worldPosition.x / cellSize),
             Mathf.RoundToInt(worldPosition.z / cellSize)
         );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GameObject.Instantiate(debugPrefab, GetWorldPosition(x, z), Quaternion.identity);
+            }
+        }
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Transform gridDebugObjectPrefab;
     private GridSystem gridSystem;
 
     // Start is called before the first frame update
     void Start()
     {
         gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
         Debug.Log(new GridPosition(5, 7));
     }
 
