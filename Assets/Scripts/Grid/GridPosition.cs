54fa774785fcc72e25430a35bd8496954d7b974c
finalmegaman NA <finalmegaman@gmail.com>
2023-03-04 13:41:05 -0800

    35 Move Action.

--- Assets/Scripts/Grid/GridPosition.cs
+++ Assets/Scripts/Grid/GridPosition.cs
@@ -39,4 +39,12 @@ public struct GridPosition: IEquatable<GridPosition> {
     public static bool operator != (GridPosition a, GridPosition b) {
         return !(a == b);
     }
+
+    public static GridPosition operator +(GridPosition a, GridPosition b) {
+        return new GridPosition(a.x + b.x, a.z + b.z);
+    }
+
+    public static GridPosition operator -(GridPosition a, GridPosition b) {
+        return new GridPosition(a.x - b.x, a.z - b.z);
+    }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
-{
-    [SerializeField]
-    private Animator unitAnimator;
-    private Vector3 targetPosition;
+public class Unit : MonoBehaviour {    
     private GridPosition gridPosition;
+    private MoveAction moveAction;
 
     private void Awake() {
-        targetPosition = transform.position;
+        moveAction = GetComponent<MoveAction>();
     }
 
     private void Start() {
@@ -18,23 +15,7 @@ public class Unit : MonoBehaviour
         LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
-    private void Update()
-    {
-        float stoppingDistance = .1f;
-
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            float moveSpeed = 4f;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-            float rotateSpeed = 10f;
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-
-            unitAnimator.SetBool("IsWalking", true);
-        } else {
-            unitAnimator.SetBool("IsWalking", false);
-        }
-
+    private void Update() {
         GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
         if (newGridPosition != gridPosition) {
             LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
@@ -42,7 +23,11 @@ public class Unit : MonoBehaviour
         }
     }
 
-    public void Move(Vector3 targetPosition) {
-        this.targetPosition = targetPosition;
+    public MoveAction GetMoveAction(){
+        return moveAction;
+    }
+
+    public GridPosition GetGridPosition() {
+        return gridPosition;
     }
 }
