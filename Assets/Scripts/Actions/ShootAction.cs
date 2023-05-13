5678141c7d5637e70b7506e0e68c339005ec4853
finalmegaman NA <finalmegaman@gmail.com>
2023-05-13 14:16:37 -0700

    52 Shooting Bullet Visual.

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
-    public event EventHandler OnShoot;
+    public event EventHandler<OnShootEventArgs> OnShoot;
+
+    public class OnShootEventArgs : EventArgs {
+        public Unit targetUnit;
+        public Unit shootingUnit;
+    }
 
     private enum State {
         Aiming,
@@ -48,7 +53,10 @@ public class ShootAction : BaseAction
     }
 
     private void Shoot(){
-        OnShoot?.Invoke(this, EventArgs.Empty);
+        OnShoot?.Invoke(this, new OnShootEventArgs {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
         targetUnit.Damage();
     }
 
--- Assets/Scripts/UnitAnimator.cs
+++ Assets/Scripts/UnitAnimator.cs
@@ -6,6 +6,8 @@ using System;
 public class UnitAnimator : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform bulletProjectilePrefab;
+    [SerializeField] private Transform shootPointTransform;
 
     private void Awake() {
         if (TryGetComponent<MoveAction>(out MoveAction moveAction)) {
@@ -26,7 +28,16 @@ public class UnitAnimator : MonoBehaviour
         animator.SetBool("IsWalking", false);
     }
 
-    private void ShootAction_OnShoot(object sender, EventArgs e) {
+    private void ShootAction_OnShoot(object sender, ShootAction.OnShootEventArgs e) {
         animator.SetTrigger("Shoot");
+
+        Transform bulletProjectileTransform = Instantiate(bulletProjectilePrefab, shootPointTransform.position, Quaternion.identity);
+
+        BulletProjectile bulletProjectile = bulletProjectileTransform.GetComponent<BulletProjectile>();
+        
+        Vector3 targetUnitShootAtPosition = e.targetUnit.GetWorldPosition();
+        targetUnitShootAtPosition.y = shootPointTransform.position.y;
+
+        bulletProjectile.Setup(targetUnitShootAtPosition);
     }
 }
