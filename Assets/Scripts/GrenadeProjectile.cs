ce122b527e6c99448ac2a682848944a781e51889
finalmegaman NA <finalmegaman@gmail.com>
2023-10-07 14:40:47 -0700

    71 Grenade Action.

---
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GrenadeProjectile : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Action onGrenadeBehaviorComplete;
+
+    private void Update() {
+        Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+        float moveSpeed = 15f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float reachedTargetDistance = .2f;
+
+        if (Vector3.Distance(transform.position, targetPosition) < reachedTargetDistance) {
+            float damageRadius = 4f;
+
+            Collider[] colliderArray = Physics.OverlapSphere(targetPosition, damageRadius);
+
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent<Unit>(out Unit targetUnit)) {
+                    targetUnit.Damage(30);
+                }
+            }
+
+            Destroy(gameObject);
+            onGrenadeBehaviorComplete();
+        }
+    }
+
+    public void Setup(GridPosition targetGridPosition, Action onGrenadeBehaviorComplete) {
+        targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+        this.onGrenadeBehaviorComplete = onGrenadeBehaviorComplete;
+    }
+}
