1da8f30fa4e4dda5b74dace0344833f650cd40b3
finalmegaman NA <finalmegaman@gmail.com>
2023-10-07 13:29:59 -0700

    70 Screen Shake.

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
+    public static event EventHandler<OnShootEventArgs> OnAnyShoot;
     public event EventHandler<OnShootEventArgs> OnShoot;
 
     public class OnShootEventArgs : EventArgs {
@@ -59,6 +60,11 @@ public class ShootAction : BaseAction
     }
 
     private void Shoot(){
+        OnAnyShoot?.Invoke(this, new OnShootEventArgs {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
+
         OnShoot?.Invoke(this, new OnShootEventArgs {
             targetUnit = targetUnit,
             shootingUnit = unit
---
+++ Assets/Scripts/ScreenShakeActions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeActions : MonoBehaviour
+{
+    private void Start() {
+        ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
+    }
+
+    private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgs e)
+    {
+        ScreenShake.Instance.Shake();
+    }
+}
