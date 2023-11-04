796f2bdb6df48e8efc6809a9e3e1a38f7d46857e
finalmegaman NA <finalmegaman@gmail.com>
2023-11-04 14:58:02 -0700

    74 Destructible Crate Parts.

--- Assets/Scripts/DestructableCrate.cs
+++ Assets/Scripts/DestructableCrate.cs
@@ -7,6 +7,7 @@ public class DestructableCrate : MonoBehaviour
 {
     public static event EventHandler OnAnyDestroyed;
     private GridPosition gridPosition;
+    [SerializeField] private Transform crateDestroyedPrefab;
 
     private void Start() {
         gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
@@ -18,7 +19,20 @@ public class DestructableCrate : MonoBehaviour
 
     public void Damage() {
         OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+        
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, transform.rotation);
+        ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position, 10f);
 
         Destroy(gameObject);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange) {
+        foreach(Transform child in root) {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody)) {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
