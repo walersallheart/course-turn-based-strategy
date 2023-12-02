269cf4157510fcb005040b284b301dfdb3b250bb
finalmegaman NA <finalmegaman@gmail.com>
2023-12-02 14:53:27 -0800

    78 Input Refactoring.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -51,7 +51,7 @@ public class UnitActionSystem : MonoBehaviour
     }
 
     private void HandleSelectedAction() {
-        if (!Input.GetMouseButtonDown(0)) {
+        if (!InputManager.Instance.IsMouseButtonDown()) {
             return;
         }
 
@@ -83,8 +83,8 @@ public class UnitActionSystem : MonoBehaviour
     }
 
     private bool TryHandleUnitSelection(){
-        if (Input.GetMouseButtonDown(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (InputManager.Instance.IsMouseButtonDown()) {
+            Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
             if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitPlaneLayerMask)) {
                 if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit)) {
                     if (unit == selectedUnit) {
